0eadec12b3b467acc7f4f35e5bc19bde2f659833
fangs073 <stephen.van.gordon@gmail.com>
2016-07-20 21:07:32 -0600

    Turned loadAuth into loadUser, replaced the isLoggedIn MW with authorizedMW

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app, passport) {
 
   app.use('/test', testRoutes);
 
-  app.get('/api/test', isLoggedIn, (req, res) => {
+  app.get('/api/test', authorize, (req, res) => {
     console.log('api/test callback hit')
     res.send('yo dog')
   });
@@ -36,18 +36,8 @@ module.exports = function(app, passport) {
   // })
 
   // route for showing the profile page
-  app.get('/loadAuth', function(req, res) {
-    console.log('/loadAuth hit');
-    if (req.isAuthenticated()) {
-      console.log('/loadAuth req authorized')
-      res.send({
-        user : req.user // get the user out of session and pass to template
-      });
-    } else {
-      console.log('req.isAuth ==', req.isAuthenticated());
-      res.send(401, 'no login found');
-    }
-
+  app.get('/loadUser', authorize, function(req, res) {
+      res.send(req.user);  // get the user out of session and pass to template
   });
 
   // route for logging out
@@ -73,13 +63,13 @@ module.exports = function(app, passport) {
 };
 
 // route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-  console.log('user logged in');
-
-  // if user is authenticated in the session, carry on
-  if (req.isAuthenticated())
-    return next();
-
-  // if they aren't redirect them to the home page
-  res.redirect('/');
+function authorize(req, res, next) {
+  console.log('authorizing');
+  if (req.isAuthenticated()) {
+    console.log('/loadAuth req authorized');
+    next();
+  } else {
+    console.log('req.isAuth ==', req.isAuthenticated());
+    res.send(401, 'not authorized');
+  }
 }
